36dd177f3884ff84f52ad5e8c9863cbac14067da
Juan José Besa <jbesa@wholemeaning.com>
2020-05-10 00:18:17 -0400

    Nominal Tests

---
+++ internal/data/memory/nominal_test.go
@@ -0,0 +1,85 @@
+package memory_test
+
+import (
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("Add", func() {
+
+	Describe("1 document",func(){
+		Context("with 1 nominal field",func(){
+			Context("with 1 value per field",func(){
+				It("the document must exist",func(){
+
+				})
+				It("the document must be the only one",func(){
+
+				})
+			})
+			Context("with 5 value per field",func(){
+				It("the document must exist for each value",func(){
+
+				})
+				It("the document must be the only one for each value",func(){
+
+				})
+			})
+		})
+		Context("with 10 nominal fields",func(){
+			Context("with 1 value per field",func(){
+				It("the document must exist for each value",func(){
+
+				})
+				It("the document must be the only one for each value",func(){
+
+				})
+			})
+			Context("with 5 value per field",func(){
+				It("the document must exist for each value",func(){
+
+				})
+				It("the document must be the only one for each value",func(){
+
+				})
+			})
+		})
+	})
+	Describe("100000 documents",func(){
+		Context("with 1 nominal field",func(){
+			Context("with 1 value per field",func(){
+				It("the documents must exist",func(){
+
+				})
+				It("the documents must be the only ones",func(){
+
+				})
+			})
+			Context("with 5 value per field",func(){
+				It("the documents must exist for each value",func(){
+
+				})
+				It("the documents must be the only ones for each value",func(){
+
+				})
+			})
+		})
+		Context("with 10 nominal fields",func(){
+			Context("with 1 value per field",func(){
+				It("the documents must exist for each value",func(){
+
+				})
+				It("the documents must be the only ones for each value",func(){
+
+				})
+			})
+			Context("with 5 value per field",func(){
+				It("the documents must exist for each value",func(){
+
+				})
+				It("the documents must be the only ones for each value",func(){
+
+				})
+			})
+		})
+	})
+})
